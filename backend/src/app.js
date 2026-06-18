f5d2272330cb14f3fd5d0c2bcc17b1087ba71c25
Sunnyburma <kanishburman111@gmail.com>
2026-06-19 02:28:12 +0530

    some changes on url

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ import recordRoutes from './routes/recordRoutes.js';
 
 const app = express();
 
-const allowedOrigins = (process.env.CLIENT_URL || 'mern-crud-app-frontend-eight.vercel.app') 
+const allowedOrigins = (process.env.CLIENT_URL || 'http://mern-crud-app-frontend-eight.vercel.app') 
 //http://localhost:5173
   .split(',')
   .map((origin) => origin.trim());
